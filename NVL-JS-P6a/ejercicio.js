7809183fe63aa18d1b458be525dd2d5487c68040
sepu85 <sepu85@gmail.com>
2019-03-09 16:45:56 +0100

    ejercicio 6a

---
+++ NVL-JS-P6a/ejercicio.js
@@ -0,0 +1,6 @@
+var miNombre="Gustavo";
+var miApellido1="Segovia";
+var miApellido2="Roa";
+var diasSemana=[lunes,martes,miercoles,jueves,viernes,sabado,domingo];
+var g=9.18;
+var numerosImpares=[1,3,5];
