2cd0d48061139ff75d19b87b317ab37f4b0e5df7
sepu85 <sepu85@gmail.com>
2019-03-11 10:49:02 +0100

    subiendo el ejercicio

---
+++ NVL-JS-P9a/ejercicio.js
@@ -0,0 +1,12 @@
+// Crea un array donde cada elemento sea un lista de dos valores.
+var myArray = [[0,1],[2,3],[4,5]];
+
+// Indica la longitud del array
+alert(myArray.length);
+
+// Indica la longitud de uno de los elementos del array
+alert(myArray[0].length);
+
+// Muestra por pantalla el valor del tercer elemento de la lista del array. Y dentro de ese tercer elemento, su segundo elemento.
+alert(myArray[2]);
+alert(myArray[2][1]);
