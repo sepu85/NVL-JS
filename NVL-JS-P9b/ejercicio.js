edda83f9a3cf47e556d182f9ed419915f87f7e1e
sepu85 <sepu85@gmail.com>
2019-03-11 11:00:46 +0100

    subida

---
+++ NVL-JS-P9b/ejercicio.js
@@ -0,0 +1,8 @@
+var dias = "Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo";
+var ciudades = "Madrid.Sevilla.Barcelona.Valencia.Zaragoza.Bilbao.Donosti"
+
+
+var arrayDias = dias.split(',');
+var arrayCiudades = ciudades.split(',');
+
+var arregloFinal = [[arrayDias[0],arrayCiudades[0]],[arrayDias[1],arrayCiudades[1]],[arrayDias[2],arrayCiudades[2]],[arrayDias[3],arrayCiudades[3]],[arrayDias[4],arrayCiudades[4]],[arrayDias[5],arrayCiudades[5]],[arrayDias[6],arrayCiudades[6]],]
